d831b400fe5ec73ec151f8f4d0a2d96cd14c2c37
Mohamed Mihammi <momihamm@e3r5p6.1337.ma>
2023-10-05 13:29:11 +0100

    feat : parssing is complet

--- philo/philo.c
+++ philo/philo.c
@@ -3,24 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: momihamm <momihamm@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: momihamm <momihamm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 03:13:22 by momihamm          #+#    #+#             */
-/*   Updated: 2023/09/19 03:20:34 by momihamm         ###   ########.fr       */
+/*   Updated: 2023/10/05 11:30:33 by momihamm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	mr(void)
+{
+	system ("leaks philo");
+}
+
 int main(int ac, char **av)
 {
-    int i=0;
-    (void) ac;
-    while (i>=0)
-    {
-        printf ("%s",av[i]);
-        if (av[i + 1] == NULL)
-            return (0);
-        i++;
-    }
+	// t_vars *param;
+	t_philo	*philo;
+	int indx;
+	atexit (mr);
+	if (ac == 5 || ac == 6)
+	{
+		// param = malloc (sizeof (t_vars));
+		philo = malloc (sizeof (t_philo));
+		philo->number_of_times_each_philosopher_must_eat = -1;
+		indx = 1;		
+		while (indx < ac)
+		{
+			if (ft_args(indx, philo, av[indx]) == 0)
+				indx++;
+			else
+			{
+				free (philo);
+				return (0);
+			}
+		}
+		printf ("number_of_philosophers  %d\n",philo->number_of_philosophers);
+		printf ("time_to_die  %d\n",philo->time_to_die);
+		printf ("time_to_eat  %d\n",philo->time_to_eat);
+		printf ("time_to_sleep  %d\n",philo->time_to_sleep);
+		printf ("number_of_times_each_philosopher_must_eat  %d\n",philo->number_of_times_each_philosopher_must_eat);
+		// free (philo);
+		// free (param);
+	}
 }
--- philo/philo.h
+++ philo/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: momihamm <momihamm@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: momihamm <momihamm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 03:02:09 by momihamm          #+#    #+#             */
-/*   Updated: 2023/09/19 03:02:13 by momihamm         ###   ########.fr       */
+/*   Updated: 2023/10/05 11:28:24 by momihamm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,41 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+
+/*********************************parssing*************************************/
+typedef struct var_s
+{
+    char	*virgin;
+	char	*ptr;
+	char	**n_virgin;
+	// int		num_of_ac;
+}t_vars;
+/******************************************************************************/
+/**************************************philo***********************************/
+typedef struct philo
+{
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_times_each_philosopher_must_eat;
+}t_philo;
+
+/******************************************************************************/
+
+// char	*ft_strjoin(char const *s1, char const *s2);
+long long	ft_atoi(char *str);
+int	ft_strlen(char *str);
+char	*ft_strdup(char *s1);
+void	*ft_calloc(size_t count, size_t size);
+void	*ft_memcpy(void *to, const void *from, size_t len);
+// char	**ft_split(char *s, char c);
+// int	num_of_word(char *s, char c);
+// void	free_all(char **s, int ptrs);
+// char	*ft_substr(char *s, int start, int len);
+int check_arg (char *str);
+int	ft_args (int indx, t_philo *philo, char *str);
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
 
 #endif
